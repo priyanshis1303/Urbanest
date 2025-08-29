5aef1e95da803c20d4e5300a64cfea13dcd6dd91
priyanshis1303 <priya.singhs1303@gmail.com>
2025-08-29 16:24:10 +0530

    index route

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express =require ("express");
 const app= express();
 const mongoose =require("mongoose");
 const MONGO_URL="mongodb://127.0.0.1:27017/Urbanest";
+const Listing =require("./models/listings.js")
 
+const path=require("path");
 main()
     .then(()=>{
         console.log("connected to DB");
@@ -18,8 +20,19 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+
+
+//views
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+//index route
 app.get(("/"),(req,res)=>{
-    res.send("on root ");
+  res.send("on root ")
+})
+app.get(("/listings"),async (req,res)=>{
+    const allisting =await Listing.find({})
+    res.render("listings/index.ejs",{  allisting});
 })
 app.listen(3000,()=>{
     console.log("app is listening to port 3000")
